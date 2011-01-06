e0a32108d9dee0a9d505881bdffe82a9658d2443
Nick England <nickengland@gmail.com>
2011-01-06 14:30:39 +0000

    added new files needed to replace test helpers from CMLXom

---
+++ src/test/java/org/xmlcml/euclid/test/EuclidTestUtils.java
@@ -0,0 +1,10 @@
+package org.xmlcml.euclid.test;
+
+import org.apache.log4j.Logger;
+
+public class EuclidTestUtils {
+	/** logger */
+	public final static Logger logger = Logger.getLogger(EuclidTestUtils.class.getName());
+	public final static String BASE_RESOURCE = "org/xmlcml/euclid";
+
+}
