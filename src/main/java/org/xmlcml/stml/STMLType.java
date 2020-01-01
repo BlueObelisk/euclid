aa11a1bb59c16b15dfbaba264ed07cfe398c240a
Peter Murray-Rust <pm286@cam.ac.uk>
2020-01-01 13:43:56 -0330

    add license headters to (almost) all files

--- src/main/java/org/xmlcml/stml/STMLType.java
+++ src/main/java/org/xmlcml/stml/STMLType.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2011 Peter Murray-Rust et. al.
+ *    Copyright 2011 Peter Murray-Rust
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package org.xmlcml.stml;
 
 import java.text.NumberFormat;
